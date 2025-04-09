9a850ee7e70e5a35608ee83f0ee6115a728477ba
T1maty <ttitaron@gmail.com>
2025-04-09 15:05:08 +0300

    add ingress to kubernetes, and refactoring main page

--- manager-password/main.py
+++ manager-password/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://localhost:8006"],
+    allow_origins=["https://localhost:8000", "http://localhost:8006"],
     allow_credentials=True,
     allow_methods=["GET","POST", "PUT", "DELETE"],
     allow_headers=["*"],
