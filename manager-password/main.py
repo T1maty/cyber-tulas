c8e6b7a8e528c7c0798ec913f55d44a9a86f2d31
T1maty <ttitaron@gmail.com>
2025-04-25 17:21:53 +0300

    Add CORS

--- manager-password/main.py
+++ manager-password/main.py
@@ -17,6 +17,15 @@ app = FastAPI()
 
 origins = ["http://localhost","http://localhost:8000"]
 
+@app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+)
+
+
 
 @app.middleware("http")
 async def add_process_time_header(request, call_next):
