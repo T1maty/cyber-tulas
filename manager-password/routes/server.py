95a85b523edcc4b7fd8b5be4ba770868e67136ee
T1maty <ttitaron@gmail.com>
2025-05-19 21:02:58 +0300

    Adding delete server method

--- manager-password/routes/server.py
+++ manager-password/routes/server.py
@@ -3,8 +3,12 @@ from models.server import ServerBaseCreate
 from models.server import ServerResponse
 from database import server_collection
 from bson import ObjectId
+import logging
 
 router = APIRouter()
+
+
+logger = logging.getLogger(__name__)
 
 @router.post("/create-server", response_model=ServerBaseCreate)
 async def create_server(server: ServerResponse):
@@ -17,3 +21,15 @@ async def create_server(server: ServerResponse):
 async def get_servers(owner_id: str):
     servers = await server_collection.find({"owner_id": ObjectId(owner_id)}).to_list(100)
     return servers
+
+
+@router.delete("/api/servers/{id}")
+async def delete_user(id: str):
+    try:
+        result = await server_collection.delete_one({"_id": ObjectId(id)})
+        if result.deleted_count == 0:
+            raise HTTPException(status_code=404, detail="Server not found")
+    except Exception as e:
+        logger.error(f"Error deleting user: {e}")
+        raise HTTPException(status_code=500, detail="Internal Server Error")
+    return {"message": "Server successfully deleted"}
