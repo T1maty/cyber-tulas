db2cd4230080c804764312aea58530e88035df9a
root <root@DESKTOP-KSDQQI4>
2025-06-30 20:29:35 +0300

    Refactoring something files

---
+++ home/user/fastapi_crud/manager-password/database.py
@@ -0,0 +1,37 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+Base = declarative_base()
+
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_HOST = os.getenv("MONGO_HOST")
+MONGO_DB = os.getenv("MONGO_DB")
+MONGO_PORT = os.getenv("MONGO_PORT", "27017")
+
+logger.info(f"MONGO_USER: {MONGO_USER}, MONGO_PASSWORD: {MONGO_PASSWORD}, MONGO_HOST: {MONGO_HOST}, MONGO_DB: {MONGO_DB}")
+
+mongo_details = f"mongodb+srv://{MONGO_USER}:{MONGO_PASSWORD}@{MONGO_HOST}/{MONGO_DB}?retryWrites=true&w=majority"
+
+try:
+    client = AsyncIOMotorClient(mongo_details)
+    database = client[MONGO_DB]
+    user_collection = database.get_collection("users")
+    server_collection = database.get_collection("servers")
+    payment_collection = database.get_collection("payments")
+    # Test the connection
+    logger.info("MongoDB connection established successfully.")
+except Exception as e:
+    logger.error(f"Error connecting to MongoDB: {e}")
+
+
+
